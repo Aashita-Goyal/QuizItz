b0febafdcb6acb8ed40da9cdf3a16e334d2827ef
aparna <aparna19shah@gmail.com>
2022-05-05 01:59:22 +0530

    as

--- OTHER/StudyMaterials/daa.php
+++ OTHER/StudyMaterials/daa.php
@@ -25,30 +25,10 @@
     </div>
   </div>
 
-<<<<<<< HEAD
- <!--Footer-->
- <footer>
-    <div class="container-fluid p-lg-3">
-      <h5 class="ps-2 mt-5">Follow Us</h5>
-      <a href="" class="icons"><i class="fa-brands fa-facebook-f" style="color: white"></i></a>
-      <a href="" class="icons"><i class="fa-brands fa-instagram" style="color: white"></i></a>
-      <a href="" class="icons"><i class="fa-brands fa-twitter" style="color: white"></i></a>
-      <br>
-      <br>
-      <p class="pt-lg-0">© 2022 QuizItz Inc</p>
-    </div>
-  </footer>
-
-
-  
-
-
-
- 
-=======
   <br><br><br>
   <?php include '../../includes/footer.php'; ?>
  </body>
 
 </html>
->>>>>>> ef27c25cb7fbaced28f561d5a8f02d3e90ca303f
+
+
